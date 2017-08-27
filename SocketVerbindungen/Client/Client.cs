c05a6e498c4f9ed4b8b8e65d457c022f518d1c64
MINATI\Jan-Niklas <jan-niklas.schroeder@minati.de>
2017-08-27 13:26:56 +0200

    Initial Commit. First Version of TCP Communication

---
+++ SocketVerbindungen/Client/Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Client");
+                Console.WriteLine("\nText zum senden eingeben");
+                string eingabe = Console.ReadLine();
+                ClientStart("127.0.0.1", eingabe);
+
+        }
+
+        static void ClientStart(string server, string message)
+        {
+
+            // Create a TcpClient.
+            // Note, for this client to work you need to have a TcpServer 
+            // connected to the same address as specified by the server, port
+            // combination.
+            Int32 port = 9000;
+            TcpClient client = new TcpClient(server, port);
+
+            // Get a client stream for reading and writing.
+            NetworkStream stream = client.GetStream();
+
+                   
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                string responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+                
+                // Close everything.
+                stream.Close();          
+                client.Close();
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+
+    }
+}
---
+++ SocketVerbindungen/ConsoleApp2/Server.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace Server
+{
+    class Server
+    {
+        public void ServerStart()
+        {
+            Console.WriteLine("Server");
+
+            IPAddress ipadr = IPAddress.Parse("127.0.0.1");
+
+            TcpListener tcpListener = new TcpListener(ipadr, 9000);
+            tcpListener.Start();
+            
+
+            while(true)
+            {
+                Socket socket = tcpListener.AcceptSocket();
+
+                if (socket.Connected)
+                {
+                    Console.WriteLine("Connection accepted" + socket.RemoteEndPoint);
+                    NetworkStream stream = new NetworkStream(socket);
+                    
+                    if (stream.DataAvailable)
+                    {
+                        //Read the Stream and display the received String
+                        Byte[] receiveData = new Byte[256];
+                        string receiveMessage = String.Empty;
+
+                        Int32 bytes = stream.Read(receiveData, 0, receiveData.Length);
+                        receiveMessage = System.Text.Encoding.ASCII.GetString(receiveData, 0, bytes);
+                        Console.WriteLine("Received: {0}", receiveMessage);
+
+                        //Send back the received string + "Echo"
+                        string responseMessage = "Echo" + receiveMessage;
+                        Console.WriteLine("Response: {0}", responseMessage);
+                        Byte[] responseData = System.Text.Encoding.ASCII.GetBytes(responseMessage);
+                        stream.Write(responseData, 0, responseData.Length);
+                       
+                    } 
+                    
+                    stream.Close();
+                    socket.Disconnect(false);
+                }
+            }
+           
+
+           
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Server server1 = new Server();
+                server1.ServerStart();
+                       
+
+        }
+    }
+}
